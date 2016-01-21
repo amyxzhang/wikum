8735b5862486684822dd465452dcb66c074e6280
Amy Zhang <axz@mit.edu>
2016-01-21 17:40:04 -0500

    add wsgi path

--- wikum/wikum/settings.py
+++ wikum/wikum/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.insert(0, BASE_DIR)
+sys.path.insert(0, os.path.join(BASE_DIR, "website"))
 
 try:
     execfile(BASE_DIR + '/private.py')
--- wikum/wikum/wsgi.py
+++ wikum/wikum/wsgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
+import sys
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wikum.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 application = get_wsgi_application()
