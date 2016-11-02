2438d4bdef50b294441b6942130c980205d260c1
Amy Zhang <axz@mit.edu>
2016-11-02 12:20:33 -0400

    edit db

--- wikum/wikum/settings.py
+++ wikum/wikum/settings.py
@@ -160,7 +160,7 @@ BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 # List of modules to import when celery starts.
 CELERY_IMPORTS = ("website.tasks",)
 CELERY_RESULT_BACKEND = "database"
-CELERY_RESULT_DBURI = "mysql://root:koob@localhost/celery"
+CELERY_RESULT_DBURI = "db+mysql://root:koob@localhost/celery"
 
 
 # Internationalization
