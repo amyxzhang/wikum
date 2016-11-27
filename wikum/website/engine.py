9f1b76dc99e7abaa843301a0e095904933ee3857
Amy Zhang <axz@mit.edu>
2016-11-27 02:36:02 -0500

    edit

--- wikum/website/engine.py
+++ wikum/website/engine.py
@@ -14,7 +14,7 @@ import pickle
 import praw
 from website.import_data import get_disqus_posts, get_reddit_posts,\
     count_replies
-from website.models import Article, Comment
+from website.models import Article, Comment, History
 
 stemmer = SnowballStemmer("english")
 stop = stopwords.words('english')
