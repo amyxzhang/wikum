c45f9e348be99699f621c08c9a1ac9faa361e8d7
elaragon <elaragon@gmail.com>
2017-11-07 21:00:14 +0100

    decide testing

--- wikum/website/import_data.py
+++ wikum/website/import_data.py
@@ -14,10 +14,6 @@ COMMENTS_CALL = 'https://disqus.com/api/3.0/threads/listPosts.json?api_key=%s&th
 
 DECIDE_CALL = '{ proposal(id: %s) { id cached_votes_up comments_count confidence_score description external_url geozone { id name } hot_score public_author { id username } public_created_at retired_at retired_explanation retired_reason summary tags(first: 10) { edges { node { id name } } } comments(first: 50, after: "%s") { pageInfo { hasNextPage endCursor } edges { node { public_author { id username } body ancestry } } } title video_url } }'
 
-
-    
-
-
 _CLOSE_COMMENT_KEYWORDS =  [r'{{(atop|quote box|consensus|Archive(-?)( ?)top|Discussion( ?)top|(closed.*?)?rfc top)', r'\|result=', r"(={2,3}|''')( )?Clos(e|ing)( comment(s?)|( RFC)?)( )?(={2,3}|''')" , 'The following discussion is an archived discussion of the proposal' , 'A summary of the debate may be found at the bottom of the discussion', 'A summary of the conclusions reached follows']
 _CLOSE_COMMENT_RE = re.compile(r'|'.join(_CLOSE_COMMENT_KEYWORDS), re.IGNORECASE|re.DOTALL)
 
@@ -531,6 +527,7 @@ def import_disqus_posts(result, article):
                                              )
         
     return count
+
 
 def import_decide_proposal_posts(result, article):
     count = 0
