e01d8ce47d9a73b4736af2556b4a2b3293eda87c
julioremo <et.julio@gmail.com>
2017-11-08 17:28:34 +0100

    Update models.py
    add a new column called 'suggested_tags' to the database, to be populated later

--- wikum/website/models.py
+++ wikum/website/models.py
@@ -113,6 +113,7 @@ class Comment(models.Model):
     vector = models.BinaryField()
     
     tags = models.ManyToManyField(Tag)
+    suggested_tags = models.ManyToManyField(Tag)
 
     def __unicode__(self):
         return 'Comment by %s on %s' % (self.author, self.article.title)
