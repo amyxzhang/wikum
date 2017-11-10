a957bd84821ed1bc6c70e4773a1d39e12901ed31
alexter <alexter@debian>
2017-11-10 05:07:38 -0600

    Edit db settings

--- wikum/private.py
+++ wikum/private.py
@@ -1,9 +1,9 @@
 MYSQL_PROD = {
         'NAME' : 'wikum',
-        'USER' : 'alexter',
-        'PASSWORD' : 'lolexie',
+        'USER' : 'myUser',
+        'PASSWORD' : 'myPassword',
         'HOST' : 'localhost',  
     }
 
-SECRET_KEY = 'longstringggggggggggggggggggggggggg'
+SECRET_KEY = ''
 DISQUS_API_KEY = ''
