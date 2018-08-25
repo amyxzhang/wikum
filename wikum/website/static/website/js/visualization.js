03180fe57d0f80be9a457c353afa875a56f84ef7
Amy Zhang <axz@mit.edu>
2018-08-25 14:33:26 -0400

    add filter

--- wikum/website/static/website/js/visualization.js
+++ wikum/website/static/website/js/visualization.js
@@ -291,7 +291,7 @@ function make_dropdown() {
 	    return false;
 	});
 	
-  var url = "/visualization_flags?article=" + article_url + '&num=' + num + '&owner=' + owner + '&sort=';
+  var url = "/visualization_flags?article=" + article_url + '&num=' + num+ '&filter=' + filter + '&owner=' + owner + '&sort=';
   $('#menu-sort').children().eq(0).children().first().attr('href', String(url + 'id'));
   $('#menu-sort').children().eq(1).children().first().attr('href', String(url + 'likes'));
   $('#menu-sort').children().eq(2).children().first().attr('href', String(url + 'replies'));
