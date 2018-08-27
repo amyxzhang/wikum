71b01e73dc7d9f247fe37d63aea59d976cd3b0b4
Amy Zhang <axz@mit.edu>
2018-08-26 21:05:21 -0400

    highlight

--- wikum/website/static/website/js/base_flags.js
+++ wikum/website/static/website/js/base_flags.js
@@ -22,6 +22,7 @@ function highlight_sents() {
 		data: data,
 		success: function(res) {
 			for (var i=0; i<res.sents.length; i++) {
+				console.log(res.sents[i]);
 				if (res.sents[i].length > 6) {
 					if (d_ids.length == 1) {
 						$('#summarize_comment_box').highlight(res.sents[i]);
