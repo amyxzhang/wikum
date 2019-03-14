837a4d609518a770b9f39ad8833736bfe1ac96ea
Sunny Tian <stian8@mit.edu>
2019-03-14 14:34:07 -0400

    Re-add private_config

---
+++ wikum/private_config.py
@@ -0,0 +1,13 @@
+MYSQL_PROD = {
+        'NAME' : 'wikum',
+        'USER' : 'myUSer',
+        'PASSWORD' : 'myPassword',
+        'HOST' : 'localhost',  
+    }
+
+SECRET_KEY = ''
+DISQUS_API_KEY = ''
+PRAW_CLIENT_SECRET = ''
+PRAW_CLIENT_ID = ''
+PRAW_USERNAME = ''
+PRAW_PASSWORD = ''
