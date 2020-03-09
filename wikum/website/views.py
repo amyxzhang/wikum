9058abf4e12e2b1386064ee58deb61ee5b5b072b
Sunny Tian <stian8@mit.edu>
2020-03-08 23:21:26 -0400

    remove print pointers

--- wikum/website/views.py
+++ wikum/website/views.py
@@ -1283,8 +1283,6 @@ def viz_data(request):
                     if current_node:
                         posts.append(current_node)
             posts = posts[start:end]
-            for c in posts:
-                print_pointers(c, a)
         elif sort == 'id':
             posts = a.comment_set.filter(reply_to_disqus=None).order_by('import_order')[start:end]
         elif sort == 'likes':
